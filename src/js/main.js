276da1eca68b2f7bc555492d265d45bee0c976bd
shariar-hriad <shariarhossainriad@gmail.com>
2022-05-15 11:23:12 +0600

    ...

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,72 @@
 // dom content loaded
 window.addEventListener('DOMContentLoaded', () => {
-  const mobileMenuButton = document.getElementById('mobileMenuButton');
-  const closeMenu = document.getElementById('closeMenu');
-  const navbar = document.getElementById('navbar');
-  const overlay = document.getElementById('overlay');
+    const mobileMenuButton = document.getElementById('mobileMenuButton');
+    const closeMenu = document.getElementById('closeMenu');
+    const navbar = document.getElementById('navbar');
+    const overlay = document.getElementById('overlay');
 
-  // toggle menu
-  mobileMenuButton.onclick = () => {
-    navbar.classList.add('showSidebar');
-    overlay.classList.add('showOverlay');
-  };
+    // toggle menu
+    mobileMenuButton.onclick = () => {
+        navbar.classList.add('showSidebar');
+        overlay.classList.add('showOverlay');
+    };
 
-  closeMenu.onclick = () => {
-    navbar.classList.remove('showSidebar');
-    overlay.classList.remove('showOverlay');
-  };
+    closeMenu.onclick = () => {
+        navbar.classList.remove('showSidebar');
+        overlay.classList.remove('showOverlay');
+    };
 
-  // cancel menu click on outside
-  document.onclick = (e) => {
-    console.log(e);
-    if (!mobileMenuButton.contains(e.target) && !closeMenu.contains(e.target)) {
-      navbar.classList.remove('showSidebar');
-      overlay.classList.remove('showOverlay');
-    }
-  };
+    // cancel menu click on outside
+    document.onclick = (e) => {
+        if (
+            !mobileMenuButton.contains(e.target) &&
+            !closeMenu.contains(e.target)
+        ) {
+            navbar.classList.remove('showSidebar');
+            overlay.classList.remove('showOverlay');
+        }
+    };
+
+    // animation initialization
+    AOS.init();
+
+    // aplic slider
+    $(document).ready(function () {
+        $('.applic__slider').owlCarousel({
+            margin: 10,
+            items: 1,
+            loop: true,
+            autoWidth: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                600: {
+                    items: 2,
+                },
+                768: {
+                    items: 2,
+                },
+                1024: {
+                    items: 3,
+                },
+                1200: {
+                    items: 4,
+                },
+            },
+        });
+    });
 });
 
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (
-  localStorage.theme === 'dark' ||
-  (!('theme' in localStorage) &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
 ) {
-  document.documentElement.classList.add('dark');
+    document.documentElement.classList.add('dark');
 } else {
-  document.documentElement.classList.remove('dark');
+    document.documentElement.classList.remove('dark');
 }
 
 // Whenever the user explicitly chooses light mode
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  content: ['./src/**/*.{html,js}'],
-  theme: {
-    container: {
-      center: true,
-      padding: '15px',
+    mode: 'jit',
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        container: {
+            center: true,
+            padding: '15px',
+        },
+        extend: {
+            backgroundImage: {
+                'dot-bg': 'url(./src/img/dot-bg.webp)',
+                'price-bg': 'url(./src/img/price.webp)',
+            },
+        },
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
